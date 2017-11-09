2aa4c6df7804877b740e57802737045d75cc9b35
shkevin <shkevin@unm.edu>
2017-11-09 13:17:52 -0700

    testing

--- lab3/server.c
+++ lab3/server.c
@@ -56,7 +56,7 @@ int main(int argc , char *argv[])
     //Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     // server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
     server.sin_port = htons( port );
 
     //Bind
