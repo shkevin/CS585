0403d44b27a4f4f7d4eca7b1c92c79ebe73c96fa
shkevin <shkevin@unm.edu>
2017-11-09 17:39:04 -0700

    testing

--- lab3/server.c
+++ lab3/server.c
@@ -99,7 +99,7 @@ int main(int argc , char *argv[])
         }
 
 		board = swapBoard(reply, board);
-		memset(reply, 0, strlen(reply));
+		// memset(reply, 0, strlen(reply));
         move = findBestMove(board, player);
 	    board[move.row][move.col] = player;
         message = sendBoard(board);
